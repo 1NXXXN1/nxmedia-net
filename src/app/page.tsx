e9a92af31a3e90b4841fdaa91b7c77a152519e00
NX <38133441+1NXXXN1@users.noreply.github.com>
2025-12-30 08:52:16 +0500

    Add dynamic export to Home component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { BestNewMovies } from '@/widgets/bestNewMovies';
 import { MovieCategoriesSection } from '@/widgets/movieCategoriesSection';
 
+export const dynamic = 'force-dynamic';
 export default function Home() {
   return (
     <article>
