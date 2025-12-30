23534fc9b5ee79e1c8777dc4a6e057b732f6472a
NX <38133441+1NXXXN1@users.noreply.github.com>
2025-12-30 08:56:52 +0500

    Fix export position of dynamic variable in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+export const dynamic = 'force-dynamic';
+
 import { BestNewMovies } from '@/widgets/bestNewMovies';
 import { MovieCategoriesSection } from '@/widgets/movieCategoriesSection';
 
-export const dynamic = 'force-dynamic';
 export default function Home() {
   return (
     <article>
